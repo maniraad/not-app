76881740f1a73f3d50ef2533a146f175cc102c29
maniraad <maniraad7@gmail.com>
2023-11-16 13:48:36 +0330

    add new note option

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const addBox = $.querySelector('.add-box'),
     popupTitle = $.querySelector('header p'),
     popupClose = $.querySelector('header i'),
     inputElem = $.querySelector('input'),
+    asideElem = $.querySelector('aside'),
     textareaElem = $.querySelector('textarea'),
     buttonElem = $.querySelector('button');
 
@@ -25,13 +26,65 @@ addBox.addEventListener('click', () => {
     inputElem.focus()
 });
 
-function closePopUp(){
+function closePopUp() {
     popupBox.classList.remove('show');
 }
 
-function generateNotes(notes) {
-    console.log(notes);
+function newNote() {
+    let newNote = {
+        tittle: inputElem.value,
+        description: textareaElem.value,
+        date: 'November 16, 2023'
+    }
+
+    notes.push(newNote)
+    setLocalStorageNotes(notes)
+    generateNotes(notes)
+    clearInputs()
+    closePopUp()
 }
+
+function clearInputs() {
+    inputElem.value = ''
+    textareaElem.value = ''
+}
+
+function generateNotes(notes) {
+
+    $.querySelectorAll('.note').forEach(note => { note.remove() })
+
+    notes.forEach(note => {
+        asideElem.insertAdjacentHTML("beforeend", `
+        <li class="note">
+            <div class="details">
+                <p>${note.tittle}</p>
+                <span>${note.description}</span>
+            </div>
+            <div class="bottom-content">
+                <span>${note.date}</span>
+                <div class="settings">
+                    <i class="uil uil-ellipsis-h" onclick="showSetting(this)"></i>
+                    <ul class="menu">
+                        <li onclick="editNote(0, '', '')">
+                            <i class="uil uil-pen"></i>Edit
+                        </li>
+                        <li onclick="removeNote(0)">
+                            <i class="uil uil-trash"></i>Delete
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </li>
+        `)
+    });
+}
+
+function showSetting(el) {
+    el.parentElement.classList.add('show')
+    console.log(el.parentElement);
+}
+
+
 
 function getLocalStorageNotes() {
     let localStorageNotes = localStorage.getItem('notes')
@@ -45,12 +98,24 @@ function getLocalStorageNotes() {
     return notes
 }
 
+function setLocalStorageNotes(notes) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+buttonElem.addEventListener('click', () => newNote())
+
 window.addEventListener('load', () => {
     let notes = getLocalStorageNotes()
     generateNotes(notes)
 })
-popupClose.addEventListener('click',()=>{closePopUp()})
-buttonElem.addEventListener('click',()=>{
-    
-    closePopUp()
+
+popupClose.addEventListener('click', () => { closePopUp() })
+
+window.addEventListener('keyup', (event) => {
+    if (event.key === 'Escape') {
+        closePopUp()
+    }
+    if (event.key === 'Enter') {
+        newNote()
+    }
 })
